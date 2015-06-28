bc3a8288c2a216ef8e9e0911e11b003fd448b6a4
unknown <emina.arapcic@bitcamp.ba>
2015-06-28 11:08:36 +0200

    homework #6

---
+++ homework02/src/ba/bitcamp/homework02/task05/Task05.java
@@ -0,0 +1,23 @@
+package ba.bitcamp.homework02.task05;
+
+public class Task05 {
+
+	public static void main(String[] args) {
+		System.out.println(getPower(2,5));
+		System.out.println(getPower(-2,4));
+		System.out.println(getPower(-2,-2));
+
+	}
+
+	private static double getPower(int a, int b) {
+		  if (b < 0 & a < 0) {
+		    	return a*1/(getPower(a,1-b));
+		    }
+		    if (b == 0) {
+		        return 1;
+		    } else {
+		        return a * getPower(a, b - 1);
+		    }
+	}
+
+}
